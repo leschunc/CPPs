087c1b628a4405fc1a0d99de8ff51a1e93bf7ed0
leschunc <leschunc@student.42lisboa.com>
2026-08-26 00:56:06 +0100

    updated ex00

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -1,33 +1,13 @@
 #include "ClapTrap.hpp"
 
-void overflowProtecDemo()
-{
-    ClapTrap ct("Claudia");
-
-    ct.beRepaired(0x7fffffed);
-    for (size_t i = 0; i < 10; i++)
-    {
-        ct.beRepaired(1);
-    }
-    ct.beRepaired(1);
-}
-
 int main()
 {
-    // overflowProtecDemo();
 
-    ClapTrap a("Roberta");
+    ClapTrap mario("mario");
 
-    for (size_t i = 0; i < 11; i++)
-        a.attack("this other gay");
-    
-    a.beRepaired(2);
-
-    ClapTrap b("Claudia");
-    for (size_t i = 0; i < 5; i++)
+    for (size_t i = 0; i < 20; i++)
     {
-        b.beRepaired(1);
-        b.takeDamage(4);
+        mario.attack("leonan");
     }
 
     return 0;
