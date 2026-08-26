4d1c316567f82521dfeb92d2e45d084319d7a104
leschunc <leschunc@student.42lisboa.com>
2026-08-26 20:03:32 +0100

    commented tests

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -62,11 +62,11 @@ void wrongCopyTest()
 int main()
 {
 
-    copyTest();
+    // copyTest();
 
-    wrongTest();
+    // wrongTest();
 
-    wrongCopyTest();
+    // wrongCopyTest();
 
     const Animal *meta = new Animal();
     const Animal *j = new Dog();
